0a42fac7820cd1f379c98708d1307bfe789e061a
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-12 16:34:31 +0000

    added /hello path

--- express_server.js
+++ express_server.js
@@ -7,8 +7,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
