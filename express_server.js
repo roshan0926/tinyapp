84b428c46512df6a7c80bcf4d67afe28af405a09
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-13 17:37:52 +0000

    added redirections part 2

--- express_server.js
+++ express_server.js
@@ -10,16 +10,41 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const generateRandomString = () => {
+  const stringList = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randomString = '';
+  for (let i = 0; i <= 5; i++) {
+    let randomNum = Math.floor(Math.random() * stringList.length);
+    let randomLetter = stringList[randomNum];
+    randomString += randomLetter;
+  }
+  return randomString;
+};
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+// app.get("/urls/:shortURL", (req, res) => {
+//   const shortURL = req.params.shortURL
+//   const longURL = urlDatabase[`${shortURL}`]
+//   const templateVars = {shortURL, longURL};
+//   res.render("urls_show", templateVars);
+// });
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL
+  const longURL = urlDatabase[`${shortURL}`]
+  const templateVars = {shortURL, longURL};
   res.render("urls_show", templateVars);
 });
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL
+  const shortURL = generateRandomString()
+  urlDatabase[`${shortURL}`] = longURL
+  console.log('...', urlDatabase);  // Log the POST request body to the console
+  res.redirect(`/u/${shortURL}`);     
 });
 app.get("/", (req, res) => {
   res.send("Hello!");
