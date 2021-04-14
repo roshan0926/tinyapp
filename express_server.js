b9a96abff8f2090147a84badaa5929f230f01246
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-14 21:07:41 +0000

    added login feature and registration

--- express_server.js
+++ express_server.js
@@ -12,6 +12,18 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "aaa"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 const generateRandomString = () => {
   const stringList = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let randomString = '';
@@ -24,24 +36,29 @@ const generateRandomString = () => {
 };
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies.user_id};
+  res.render("urls_new", templateVars);
 });
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
   const longURL = urlDatabase[`${shortURL}`]
-  const templateVars = {shortURL, longURL};
+  const templateVars = {shortURL, longURL, user_id: req.cookies.user_id};
   res.render("urls_show", templateVars);
 });
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies.username};
-  console.log(req.cookies.username)
+  let user 
+  if (req.cookies.user_id) {
+    user = users[req.cookies.user_id]
+  }
+  const templateVars = { urls: urlDatabase, user};
+  console.log('....%', user)
   res.render("urls_index", templateVars);
 });
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString()
   const longURL = req.body.longURL
   urlDatabase[`${shortURL}`] = longURL
-  console.log('...', urlDatabase);  // Log the POST request body to the console
+  console.log(urlDatabase);  // Log the POST request body to the console
   res.redirect(`/u/${shortURL}`);     
 });
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -56,15 +73,74 @@ urlDatabase[shortURL] = longURL
 res.redirect('/urls')
 });
 app.post('/logout', function (req, res) {
-  res.clearCookie('username')
+  res.clearCookie('user_id')
   res.redirect('/urls')
 })
-app.post('/login', (req, res) => {
-  const username = req.cookies.username
-  console.log(req.body)
-  res.cookie('username', req.body)
+// app.post('/login', (req, res) => {
+//   console.log(req.body)
+//   res.cookie('username', req.body)
+//   res.redirect('/urls')
+// });
+app.get('/register', (req, res) => {
+  let user = null;
+  const templateVars = {user};
+  res.render('urls_register', templateVars)
+})
+app.post('/register', (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(400).send("Error: your imput field was empty");
+  }
+  for (const lookup in users) {
+    if (req.body.email === users[lookup].email) {
+      res.status(400).send("Error: this email adress already has an account");
+    }
+  }
+  const id = generateRandomString()
+  const email = req.body.email
+  const password = req.body.password
+  users[id] ={ id, email, password }
+  res.cookie('user_id', id)
+  console.log('cookies...', req.cookies.user_id)
+  console.log('.', id)
+  // console.log('users', users)
   res.redirect('/urls')
-});
+})
+app.get('/login', (req, res) => {
+  let user = null;
+  const templateVars = {user};
+  res.render('urls_login', templateVars)
+})
+const findUserbyEmail = (email, users) => {
+  for (let lookUp in users) {
+    if (users[lookUp].email === email) {
+      return users[lookUp];
+    }
+  }
+  return undefined;
+
+
+  //   const userid = users[lookUp]
+  //   if (req.body.email !== userid.email ) {
+  //     res.status(403).send("Error: this email adress does not have an account");
+  //   }
+  //   if (req.body.email === userid.email && req.body.password !== userid.password) {
+  //     res.status(403).send("Error: incorrect password");
+  //   }
+  // }
+}
+app.post('/login', (req, res) => {
+  let email = req.body.email
+  let password = req.body.password
+  let user = findUserbyEmail(email, users)
+  if (user === undefined) {
+    res.status(403).send("Error: this email adress does not have an account");
+  }
+  else if (user.password !== password) {
+    res.status(403).send("Error: incorrect password");
+  }
+  res.cookie('user_id', user.id)
+  res.redirect('/urls')
+})
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
