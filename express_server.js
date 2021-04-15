5cb963c6bd95055c6140c4ae8c3e14bb5c80fe10
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-15 16:23:22 +0000

    linted

--- express_server.js
+++ express_server.js
@@ -3,32 +3,32 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-// const cookieParser = require("cookie-parser");
-// app.use(cookieParser())
 const bcrypt = require('bcrypt');
-cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 const saltRounds = 10;
+
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
-}))
+}));
+
 app.set("view engine", "ejs");
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "userRandomID" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "userRandomID" }
 };
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "aaa"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
-}
+};
 const generateRandomString = () => {
   const stringList = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let randomString = '';
@@ -40,72 +40,71 @@ const generateRandomString = () => {
   return randomString;
 };
 const urlsForUser = (id) => {
-  let filtered = {}
-  for (let shortURL in urlDatabase) { 
+  let filtered = {};
+  for (let shortURL in urlDatabase) {
     if (id === urlDatabase[shortURL].userID) {
-      filtered[shortURL] = {longURL: urlDatabase[shortURL].longURL}
+      filtered[shortURL] = {longURL: urlDatabase[shortURL].longURL};
     }
   }
   return filtered;
-}
+};
 
 app.get("/urls/new", (req, res) => {
-  let user = ''
+  let user = '';
   if (req.session.user_id) {
-    user = users[req.session.user_id]
+    user = users[req.session.user_id];
   }
   const templateVars = {user, userId: req.session.user_id};
   res.render("urls_new", templateVars);
 });
 app.get("/u/:shortURL", (req, res) => {
-  let user = ''
+  let user = '';
   if (req.session.user_id) {
-    user = users[req.session.user_id]
+    user = users[req.session.user_id];
   }
-  const shortURL = req.params.shortURL
-  const longURL = urlDatabase[`${shortURL}`]
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[`${shortURL}`];
   const templateVars = {shortURL, longURL, userId: req.session.user_id, user, urls: urlDatabase};
   res.render("urls_show", templateVars);
 });
 app.get("/urls", (req, res) => {
-  let urlObj = urlsForUser(req.session.user_id)
-  console.log('urlDatabse', urlDatabase)
-  let user = ''
+  let urlObj = urlsForUser(req.session.user_id);
+  console.log('urlDatabse', urlDatabase);
+  let user = '';
   if (req.session.user_id) {
-    user = users[req.session.user_id]
+    user = users[req.session.user_id];
   }
   const templateVars = { urls: urlObj, userId: req.session.user_id, user};
                         
   res.render("urls_index", templateVars);
 });
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString()
-  const longURL = req.body.longURL
-  urlDatabase[shortURL] = {longURL: longURL, userID: req.session.user_id}
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = {longURL: longURL, userID: req.session.user_id};
   console.log(urlDatabase);  // Log the POST request body to the console
-  res.redirect(`/u/${shortURL}`);     
+  res.redirect(`/u/${shortURL}`);
 });
 app.post("/urls/:shortURL/delete", (req, res) => {
-  const shortURL = req.params.shortURL
-  delete urlDatabase[shortURL]
-  res.redirect("/urls")
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
 });
 app.post("/urls/:shortURL", (req, res) => {
-const shortURL = req.params.shortURL
-const longURL = req.body.longURL
-urlDatabase[shortURL] = longURL 
-res.redirect('/urls')
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls');
 });
-app.post('/logout', function (req, res) {
-  // res.clearCookie('user_id')
-  req.session['user_id'] = null
-  res.redirect('/urls')
-})
+app.post('/logout', function(req, res) {
+  req.session['user_id'] = null;
+  res.redirect('/urls');
+});
 app.get('/register', (req, res) => {
   let user = null;
   const templateVars = {user};
-  res.render('urls_register', templateVars)
-})
+  res.render('urls_register', templateVars);
+});
 app.post('/register', (req, res) => {
   if (!req.body.email || !req.body.password) {
     res.status(400).send("Error: your imput field was empty");
@@ -116,20 +115,17 @@ app.post('/register', (req, res) => {
     }
   }
 
-  const id = generateRandomString()
-  const email = req.body.email
+  const id = generateRandomString();
+  const email = req.body.email;
   const password = bcrypt.hashSync(req.body.password, saltRounds);
-  users[id] ={ id, email, password }
-  req.session.user_id = id; //, id            *****
-  // console.log('cookies...', req.cookies.user_id)
-  // console.log('.', id)
-  // console.log('users', users)
-  res.redirect('/urls')
-})
+  users[id] = { id, email, password };
+  req.session.user_id = id;
+  res.redirect('/urls');
+});
 app.get('/login', (req, res) => {
   let user = null;
   const templateVars = {user};
-  res.render('urls_login', templateVars)
+  res.render('urls_login', templateVars);
 });
 const findUserbyEmail = (email, users) => {
   for (let lookUp in users) {
@@ -138,21 +134,19 @@ const findUserbyEmail = (email, users) => {
     }
   }
   return undefined;
-
 };
 app.post('/login', (req, res) => {
-  let email = req.body.email
-  let password = bcrypt.hashSync(req.body.password, saltRounds)
-  let user = findUserbyEmail(email, users)
+  let email = req.body.email;
+  let password = bcrypt.hashSync(req.body.password, saltRounds);
+  let user = findUserbyEmail(email, users);
   if (user === undefined) {
     res.status(403).send("Error: this email adress does not have an account");
-  }
-  else if (user.password && bcrypt.compareSync(password, user.password)) {
+  } else if (user.password && bcrypt.compareSync(password, user.password)) {
     res.status(403).send("Error: incorrect password");
   }
-  console.log(users)
-  req.session.user_id = user.id //, user.id   *****
-  res.redirect('/urls')
+  console.log(users);
+  req.session.user_id = user.id; 
+  res.redirect('/urls');
 });
 app.get("/", (req, res) => {
   res.send("Hello!");
